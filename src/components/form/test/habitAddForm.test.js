fba7a1552af668d959849e6fb93af9040882b5c4
hhk <nowhhk@gmail.com>
2022-09-23 16:04:13 +0900

    snapshot test (react-test-renderer)

--- src/components/form/test/habitAddForm.test.js
+++ src/components/form/test/habitAddForm.test.js
@@ -3,34 +3,44 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import HabitAddForm from '../habitAddForm';
+import renderer from 'react-test-renderer';
 
 describe('HabitAddForm', () => {
-  let onAdd;
-  let input;
-  let button;
+  it('renders', () => {
+    //스냅샷 테스트
 
-  beforeEach(() => {
-    onAdd = jest.fn();
-    render(<HabitAddForm onAdd={onAdd} />);
-    input = screen.getByPlaceholderText('Habit');
-    button = screen.getByText('Add');
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it('calls onAdd when button is clicked and valid habit is entered', () => {
-    //input에 원하는 habit 이름을 타이핑 한 다음에
-    //add라는 버튼을 클릭하면
-    //onAdd가 input에 입력된 이름과 함께 호출되어야함
+  describe('Form Submit', () => {
+    let onAdd;
+    let input;
+    let button;
 
-    userEvent.type(input, 'New Habit');
-    userEvent.click(button);
+    beforeEach(() => {
+      onAdd = jest.fn();
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText('Habit');
+      button = screen.getByText('Add');
+    });
 
-    expect(onAdd).toHaveBeenCalledWith('New Habit');
-  });
+    it('calls onAdd when button is clicked and valid habit is entered', () => {
+      //input에 원하는 habit 이름을 타이핑 한 다음에
+      //add라는 버튼을 클릭하면
+      //onAdd가 input에 입력된 이름과 함께 호출되어야함
 
-  it('does not call onAdd when the habit is empty', () => {
-    userEvent.type(input, '');
-    userEvent.click(button);
+      userEvent.type(input, 'New Habit');
+      userEvent.click(button);
 
-    expect(onAdd).toHaveBeenCalledTimes(0);
+      expect(onAdd).toHaveBeenCalledWith('New Habit');
+    });
+
+    it('does not call onAdd when the habit is empty', () => {
+      userEvent.type(input, '');
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledTimes(0);
+    });
   });
 });
